5b340d65628fb7fd144ab6408922572821df373d
김아현 <ahhyeonny99@gmail.com>
2024-03-19 20:07:22 +0900

    feat : QueueItem 생성

---
+++ gRPCRedisExample/GrpcService/model/QueueItem.cs
@@ -0,0 +1,14 @@
+﻿namespace GrpcService.model
+{
+    public class QueueItem
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+
+        public QueueItem(int id, string value)
+        {
+            Id = id;
+            Value = value;
+        }
+    }
+}
